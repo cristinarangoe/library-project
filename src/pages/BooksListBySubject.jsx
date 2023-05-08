80bd6aa8ed6d392312306954759df8489fa64220
Cristina Arango Escobar <cristina.arangoe@perficient.com>
2023-05-08 09:40:19 -0500

    Removing commented code

--- src/pages/BooksListBySubject.jsx
+++ src/pages/BooksListBySubject.jsx
@@ -23,38 +23,6 @@ function BooksListBySubject() {
     setBooks(transformedBooks);
     setTotalPages(Math.ceil(data.work_count / limit));
   };
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       `https://openlibrary.org/subjects/${params.subject}.json?limit=${limit}&offset=${offset}`
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error("Algo salió mal!");
-  //     }
-
-  //     const data = await response.json();
-
-  //     console.log(data)
-
-  //     const transformedBooks = data.works.map((book) => {
-  //       return {
-  //         id: book.key,
-  //         coverUrl: book.cover_edition_key && `https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-L.jpg`,
-  //         title: book.title,
-  //         authors: book.authors.map((author) => author.name).join(", "),
-  //       };
-  //     });
-
-  //     setBooks(transformedBooks);
-  //     setTotalPages(5)
-  //   } catch (error) {
-  //     setError(error.message);
-  //   }
-  //   setIsLoading(false);
-  // }, [params, limit, offset]);
-
   useEffect(() => {
     getTranformatedData();
   }, [data]);
